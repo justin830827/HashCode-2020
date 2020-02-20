defed1132e3ad1856f522b22a261bfab409fa997
Justin Hu <justin801514@gmail.com>
2020-02-20 13:27:20 -0500

    update readfile

--- main.py
+++ main.py
@@ -2,13 +2,28 @@ import sys
 
 
 def read_file(filename):
-    path = './Data/' + filename + '.in'
+    path = './Data/' + filename + '.txt'
     with open(path, 'r') as f:
-        spec = f.readline().split()
-        target, number = int(spec[0]), int(spec[1])
-        candidates = list(map(int, f.readline().split()))
+        specs = f.readline().split()
+        # Save basic specs on first line of input files.
+        n_book, n_lib, deadline = int(specs[0]), int(specs[1]), int(specs[2])
 
-    return target, number, candidates
+        # Save books with its score.
+        books = list(map(int, f.readline().split()))
+        row = 0
+
+        # Save libraies information with [signup, max_ship, books(set)]
+        libs = []
+        while n_lib > row/2:
+            if row % 2 == 0:
+                cur_lib = list(map(int, f.readline().split()))
+                libs.append(cur_lib)
+            else:
+                # append libray's books into its library_id
+                libs[-1].append(set(map(int, f.readline().split())))
+            row += 1
+
+    return (n_book, n_lib, deadline), books, libs
 
 
 def wirte(results, filename):
@@ -26,7 +41,10 @@ def main():
 
     filename = sys.argv[1]
 
-    read_file(filename)
+    specs, books, libs = read_file(filename)
+    print("Specs: {}".format(specs))
+    print("Books: {}".format(books))
+    print("Libraies: {}".format(libs))
 
 
 if __name__ == '__main__':
